de973156a541d90e963c319c297b67ec6b1579f3
teddyGrd <teddyguerrand76@gmail.com>
2024-07-10 12:48:00 +0200

    ajout de gestion d erreur

--- philo/main.c
+++ philo/main.c
@@ -6,7 +6,7 @@
 /*   By: tguerran <tguerran@student.42lehavre.fr    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/18 16:48:32 by tguerran          #+#    #+#             */
-/*   Updated: 2024/06/26 17:11:37 by tguerran         ###   ########.fr       */
+/*   Updated: 2024/07/10 12:38:17 by tguerran         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,10 @@ int	main(int argc, char *argv[])
 	t_data	data;
 
 	if (argc != 2 && argc != 5 && argc != 6)
+	{
+		printf("error argument \n");
 		return (1);
+	}
 	if (check_error(argc, argv) == 1)
 		return (1);
 	init_data(&data, argc, argv);
